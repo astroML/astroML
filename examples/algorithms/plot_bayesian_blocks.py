a910c526b308e3d3dec37ef78dda0e9b03096d0b
Jake Vanderplas <vanderplas@astro.washington.edu>
2012-10-22 00:24:39 -0700

    FIG: add bayes blocks plot

--- examples/algorithms/plot_bayesian_blocks.py
+++ examples/algorithms/plot_bayesian_blocks.py
@@ -35,15 +35,35 @@ t = np.concatenate([stats.cauchy(-5, 1.8).rvs(500),
 # truncate values to a reasonable range
 t = t[(t > -15) & (t < 15)]
 
-# plot a standard histogram in the background, with alpha transparency
-hist(t, bins=200, histtype='stepfilled',
-     alpha=0.2, normed=True, label='standard histogram')
+#------------------------------------------------------------
+# First figure: show normal histogram binning
+fig = plt.figure(figsize=(10, 4))
+fig.subplots_adjust(left=0.1, right=0.95, bottom=0.15)
 
-# plot an adaptive-width histogram on top
-hist(t, bins='blocks', color='black',
-     histtype='step', normed=True, label='bayesian blocks')
+ax1 = fig.add_subplot(121)
+ax1.hist(t, bins=15, histtype='stepfilled', alpha=0.2, normed=True)
+ax1.set_xlabel('t')
+ax1.set_ylabel('P(t)')
 
-plt.legend()
-plt.xlabel('t')
-plt.ylabel('P(t)')
+ax2 = fig.add_subplot(122)
+ax2.hist(t, bins=200, histtype='stepfilled', alpha=0.2, normed=True)
+ax2.set_xlabel('t')
+ax2.set_ylabel('P(t)')
+
+for bins in ['knuth', 'blocks']:
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+
+    # plot a standard histogram in the background, with alpha transparency
+    hist(t, bins=200, histtype='stepfilled',
+         alpha=0.2, normed=True, label='standard histogram')
+
+    # plot an adaptive-width histogram on top
+    hist(t, bins=bins, ax=ax, color='black',
+         histtype='step', normed=True, label='Knuth histogram')
+
+    ax.legend()
+    ax.set_xlabel('t')
+    ax.set_ylabel('P(t)')
+
 plt.show()
