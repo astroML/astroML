7081c1ee6ea16a0bbfde69bb9bdd803a80312239
Jake Vanderplas <vanderplas@astro.washington.edu>
2013-06-10 14:21:44 -0700

    add textbook plot settings command

--- astroML/plotting/__init__.py
+++ astroML/plotting/__init__.py
@@ -3,3 +3,4 @@ from .scatter_contour import scatter_contour
 from .mcmc import plot_mcmc
 from .ellipse import plot_tissot_ellipse
 from .multiaxes import MultiAxes
+from .settings import setup_text_plots
---
+++ astroML/plotting/settings.py
@@ -0,0 +1,15 @@
+def setup_text_plots(fontsize=8, usetex=True):
+    """
+    This function adjusts matplotlib settings so that all figures in the
+    textbook have a uniform format and look.
+    """
+    import matplotlib
+    matplotlib.rc('legend', fontsize=fontsize)
+    matplotlib.rc('axes', titlesize=fontsize)
+    matplotlib.rc('axes', labelsize=fontsize)
+    matplotlib.rc('xtick', labelsize=fontsize)
+    matplotlib.rc('ytick', labelsize=fontsize)
+    matplotlib.rc('text', usetex=usetex)
+    matplotlib.rc('font', size=fontsize, family='serif',
+                  style='normal', variant='normal',
+                  stretch='normal', weight='normal')
