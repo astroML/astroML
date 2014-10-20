57fba48de8e21103e52d9b359032f1508ec69b38
Jake VanderPlas <jakevdp@gmail.com>
2014-10-20 16:29:35 -0700

    add astroML.__bibtex__ attribute

--- astroML/__init__.py
+++ astroML/__init__.py
@@ -1,1 +1,11 @@
 __version__ = '0.3-git'
+__bibtex__ = """@INPROCEEDINGS{astroML,
+ author={{Vanderplas}, J.T. and {Connolly}, A.J.
+         and {Ivezi{\'c}}, {\v Z}. and {Gray}, A.},
+ booktitle={Conference on Intelligent Data Understanding (CIDU)},
+ title={Introduction to astroML: Machine learning for astrophysics},
+ month={Oct.},
+ pages={47 -54},
+ doi={10.1109/CIDU.2012.6382200},
+ year={2012}
+}"""
