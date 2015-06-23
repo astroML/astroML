3b09066e7eb0c344fa4e16de1ff658e661bbb268
Nicholas Hunt-Walker <nhuntwalker@gmail.com>
2015-06-23 11:35:55 -0700

    TST: tested generate_RW. Difficult to test outputs so just looked to see if the mean of the output random walk is more or less centered around the input mean

--- astroML/time_series/tests/test_generate.py
+++ astroML/time_series/tests/test_generate.py
@@ -1,6 +1,6 @@
 import numpy as np
-from numpy.testing import assert_
-from astroML.time_series import generate_power_law
+from numpy.testing import assert_, assert_almost_equal
+from astroML.time_series import generate_power_law, generate_damped_RW
 
 
 def check_generate_args(N, dt, beta, generate_complex):
@@ -16,3 +16,15 @@ def test_generate_args():
     for N in [10, 11]:
         for generate_complex in [True, False]:
             yield (check_generate_args, N, dt, beta, generate_complex)
+
+
+def test_generate_RW():
+    t = np.arange(0., 1E2)
+    tau = 300
+    z = 2.0
+    xmean = np.random.random(1)*200 - 100
+    N = len(t)
+    y = generate_damped_RW(t, tau=tau, z=z, xmean=xmean)
+
+    assert_(len(generate_damped_RW(t)) == N)
+    assert_almost_equal(np.mean(y), xmean, 0)
