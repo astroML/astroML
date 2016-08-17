e70a07d6f47c5336077192132ef1e10e15ad0ff5
Andreas Kopecky <andreas.kopecky@gmail.com>
2016-08-17 16:09:52 +0200

    Relative imports in wrong order if you want to use histogram via density_estimation module

--- astroML/density_estimation/__init__.py
+++ astroML/density_estimation/__init__.py
@@ -1,7 +1,7 @@
 from .density_estimation import KDE, KNeighborsDensity
 from .xdeconv import XDGMM
+from .bayesian_blocks import bayesian_blocks
 from .histtools import\
     scotts_bin_width, freedman_bin_width, knuth_bin_width, histogram
-from .bayesian_blocks import bayesian_blocks
 from .empirical import FunctionDistribution, EmpiricalDistribution
 from .gauss_mixture import GaussianMixture1D
