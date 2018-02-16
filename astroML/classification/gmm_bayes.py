2c85a9e266a38d64e228eab8d5ecaff68024b99c
Chris <cd15g13@soton.ac.uk>
2018-02-16 17:20:29 +0000

    In order to be compatible with new version of Scikit-Learn, GMM() replaced with GaussianMixture() 	modified:   gmm_bayes.py

--- astroML/classification/gmm_bayes.py
+++ astroML/classification/gmm_bayes.py
@@ -5,26 +5,25 @@ This implements generative classification based on mixtures of gaussians
 to model the probability density of each class.
 """
 import numpy as np
-from sklearn.mixture import GMM
 from sklearn.naive_bayes import BaseNB
+from sklearn.mixture import GaussianMixture
 
 
 class GMMBayes(BaseNB):
-    """GMM Bayes Classifier
 
+    """GMM Bayes Classifier
     This is a generalization to the Naive Bayes classifier: rather than
     modeling the distribution of each class with axis-aligned gaussians,
     GMMBayes models the distribution of each class with mixtures of
     gaussians.  This can lead to better classification in some cases.
-
     Parameters
     ----------
     n_components : int or list
         number of components to use in the gmm.  If specified as a list, it
         must match the number of class labels
-
     other keywords are passed directly to GMM
     """
+
     def __init__(self, n_components=1, **kwargs):
         self.n_components = np.atleast_1d(n_components)
         self.kwargs = kwargs
@@ -54,12 +53,14 @@ class GMMBayes(BaseNB):
         n_comp = np.zeros(len(self.classes_), dtype=int) + self.n_components
 
         for i, y_i in enumerate(unique_y):
-            self.gmms_[i] = GMM(n_comp[i], **self.kwargs).fit(X[y == y_i])
+            self.gmms_[i] = GaussianMixture(n_comp[i], **self.kwargs).fit(X[y == y_i])
             self.class_prior_[i] = np.float(np.sum(y == y_i)) / n_samples
 
         return self
 
     def _joint_log_likelihood(self, X):
+
         X = np.asarray(np.atleast_2d(X))
-        logprobs = np.array([g.score(X) for g in self.gmms_]).T
-        return logprobs + np.log(self.class_prior_)
+        logprobs = np.vstack([g.score_samples(X) for g in self.gmms_])
+        print((logprobs.T + np.log(self.class_prior_)).shape)
+        return logprobs.T + np.log(self.class_prior_)
