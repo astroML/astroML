4daabb79626586a77b7eabde1a72038488af12c3
Brigitta Sipocz <bsipocz@gmail.com>
2018-12-13 16:49:16 -0800

    Adding exceptions

---
+++ astroML/utils/exceptions.py
@@ -0,0 +1,9 @@
+"""
+This module contains errors/exceptions and warnings for astroML.
+"""
+
+
+class AstroMLDeprecationWarning(Warning):
+    """
+    A warning class to indicate a deprecated feature in astroML.
+    """
