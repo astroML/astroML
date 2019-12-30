35b3efaa322105a696a1f288e510e187abcb88b8
Brigitta Sipocz <bsipocz@gmail.com>
2019-12-30 15:23:47 -0800

    Use pytest_astropy_header

--- astroML/conftest.py
+++ astroML/conftest.py
@@ -1,13 +1,10 @@
+from pytest_astropy_header.display import (PYTEST_HEADER_MODULES,
+                                           TESTED_VERSIONS)
 
-try:
-    from astropy.tests.plugins.display import (pytest_report_header,
-                                               PYTEST_HEADER_MODULES,
-                                               TESTED_VERSIONS)
-except ImportError:
-    # When using astropy 2.0
-    from astropy.tests.pytest_plugins import (pytest_report_header,
-                                              PYTEST_HEADER_MODULES,
-                                              TESTED_VERSIONS)
+
+def pytest_configure(config):
+    config.option.astropy_header = True
+
 
 try:
     PYTEST_HEADER_MODULES['Cython'] = 'Cython'
