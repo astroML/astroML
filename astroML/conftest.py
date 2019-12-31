e9a1b0d45e223d2e45f578cd14745f111f6c61d5
Brigitta Sipocz <bsipocz@gmail.com>
2019-12-30 18:31:12 -0800

    Making pytest-astropy-header optional dependency

--- astroML/conftest.py
+++ astroML/conftest.py
@@ -1,28 +1,28 @@
-from pytest_astropy_header.display import (PYTEST_HEADER_MODULES,
-                                           TESTED_VERSIONS)
-
-
-def pytest_configure(config):
-    config.option.astropy_header = True
-
-
 try:
-    PYTEST_HEADER_MODULES['Cython'] = 'Cython'
-    PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
-    PYTEST_HEADER_MODULES['scikit-learn'] = 'sklearn'
-    # pymc import here triggers pytest INTERNALERROR thus as a temporary
-    # measure we don't list its version number
-    #    PYTEST_HEADER_MODULES['pymc'] = 'pymc'
-    PYTEST_HEADER_MODULES['astroML_addons'] = 'astroML_addons'
-    del PYTEST_HEADER_MODULES['h5py']
-    del PYTEST_HEADER_MODULES['Pandas']
-except (KeyError):
+    from pytest_astropy_header.display import (PYTEST_HEADER_MODULES,
+                                               TESTED_VERSIONS)
+
+    def pytest_configure(config):
+        config.option.astropy_header = True
+    try:
+        PYTEST_HEADER_MODULES['Cython'] = 'Cython'
+        PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
+        PYTEST_HEADER_MODULES['scikit-learn'] = 'sklearn'
+        # pymc import here triggers pytest INTERNALERROR thus as a temporary
+        # measure we don't list its version number
+        #    PYTEST_HEADER_MODULES['pymc'] = 'pymc'
+        PYTEST_HEADER_MODULES['astroML_addons'] = 'astroML_addons'
+        del PYTEST_HEADER_MODULES['h5py']
+        del PYTEST_HEADER_MODULES['Pandas']
+    except (KeyError):
+        pass
+
+    # This is to figure out the package version, rather than
+    # using Astropy's
+
+    from . import __version__ as version
+
+    TESTED_VERSIONS['astroMl'] = version
+
+except ImportError:
     pass
-
-
-# This is to figure out the package version, rather than
-# using Astropy's
-
-from . import __version__ as version
-
-TESTED_VERSIONS['astroMl'] = version
