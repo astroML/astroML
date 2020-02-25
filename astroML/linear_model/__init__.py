578d143aebf969af34bb014fb9ee9f614b6a5de4
Brigitta Sipocz <bsipocz@gmail.com>
2020-02-24 16:16:49 -0800

    Adding API for LinearRegressionwithErrors

--- astroML/linear_model/__init__.py
+++ astroML/linear_model/__init__.py
@@ -1,4 +1,4 @@
-from .linear_regression import \
-    LinearRegression, PolynomialRegression, BasisFunctionRegression
+from .linear_regression import LinearRegression, PolynomialRegression, BasisFunctionRegression
+from .linear_regression_errors import LinearRegressionwithErrors
 from .kernel_regression import NadarayaWatson
 from .TLS import TLS_logL
