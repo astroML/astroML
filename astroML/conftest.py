58952078f9986dcf5552fe4cb97a87da8ab5da49
Brigitta Sipőcz <bsipocz@gmail.com>
2021-03-16 12:45:05 -0700

    MNT: Adding theano to the test header

--- astroML/conftest.py
+++ astroML/conftest.py
@@ -5,11 +5,10 @@ try:
     def pytest_configure(config):
         config.option.astropy_header = True
     try:
-        PYTEST_HEADER_MODULES['Cython'] = 'Cython'
         PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
         PYTEST_HEADER_MODULES['scikit-learn'] = 'sklearn'
         PYTEST_HEADER_MODULES['pymc3'] = 'pymc3'
-        PYTEST_HEADER_MODULES['astroML_addons'] = 'astroML_addons'
+        PYTEST_HEADER_MODULES['Theano'] = 'theano'
         del PYTEST_HEADER_MODULES['h5py']
         del PYTEST_HEADER_MODULES['Pandas']
     except (KeyError):
